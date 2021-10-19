43e650f032deb64f36df76de8b1af550c6846324
AnthonyRyck <anthony.ryck@hotmail.fr>
2021-10-19 15:43:20 +0200

    Ajout de Serilog pour MySQL

--- src/BlogSolution/BlazorBlog/Program.cs
+++ src/BlogSolution/BlazorBlog/Program.cs
@@ -1,6 +1,7 @@
 using BlazorBlog.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -14,19 +15,6 @@ namespace BlazorBlog
 	{
 		public static void Main(string[] args)
 		{
-			string pathLog = Path.Combine(AppContext.BaseDirectory, "Logs");
-			if (!Directory.Exists(pathLog))
-			{
-				Directory.CreateDirectory(pathLog);
-			}
-
-			Log.Logger = new LoggerConfiguration()
-				.MinimumLevel.Debug()
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-				.MinimumLevel.Override("System", LogEventLevel.Warning)
-				.WriteTo.RollingFile(Path.Combine(pathLog, "log-{Date}.txt"))
-				.CreateLogger();
-
 			try
 			{
 				var host = CreateHostBuilder(args).Build();
@@ -44,6 +32,34 @@ namespace BlazorBlog
 						DataInitializer.InitData(roleManager, userManager).Wait();
 					}
 				}
+
+				// Pour les logs.
+				// ATTENTION : il faut que la table Logs (créé par Serilog) soit faites APRES
+				// la création des tables ASP, sinon "db.Database.EnsureCreated" considère que la
+				// base est déjà créée.
+				var configuration = new ConfigurationBuilder()
+							.SetBasePath(Directory.GetCurrentDirectory())
+							.AddJsonFile("appsettings.json")
+							.Build();
+
+				string connectionDb = configuration.GetConnectionString("MySqlConnection");
+
+				string databaseAddress = Environment.GetEnvironmentVariable("DB_HOST");
+				string login = Environment.GetEnvironmentVariable("LOGIN_DB");
+				string mdp = Environment.GetEnvironmentVariable("PASSWORD_DB");
+				string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+
+				connectionDb = connectionDb.Replace("USERNAME", login)
+										.Replace("YOURPASSWORD", mdp)
+										.Replace("YOURDB", dbName)
+										.Replace("YOURDATABASE", databaseAddress);
+
+				Log.Logger = new LoggerConfiguration()
+					.MinimumLevel.Debug()
+					.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+					.MinimumLevel.Override("System", LogEventLevel.Warning)
+					.WriteTo.MySQL(connectionDb, "Logs")
+					.CreateLogger();
 
 				host.Run();
 			}
