5b583265c21702fe5562280ddc538574cd33be80
Anthony RYCK <anthony.ryck@hotmail.fr>
2022-04-07 06:08:48 +0200

    Remplacement du projet par .Net6

---
+++ src/BlazorBlog/Data/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Data
+{
+	public enum Role
+	{
+		SansRole,
+		Member,
+		Manager,
+		Admin
+	}
+}
