b429eacdaa70d1c7f9ca7f90063bc72cdf53c3ca
Anthony RYCK <anthony.ryck@hotmail.fr>
2022-04-16 17:14:38 +0200

    Ajout du composant EditorMarkdown

---
+++ src/BlazorBlog/Composants/EditorMarkdown.razor.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace BlazorBlog.Composants
+{
+	partial class EditorMarkdown
+	{
+		[Parameter]
+		public string Content { get; set; }
+
+		[Parameter]
+		public EventCallback<string> ContentChanged { get; set; }
+
+
+		public MudTextField<string> Text { get; set; }
+
+		private async Task OnTextChanged(string newValue)
+		{
+			await ContentChanged.InvokeAsync(newValue);
+		}
+
+		private async void OnClickLink()
+		{
+			string value = await GetSelection();
+			string markdown = string.Empty;
+
+			if (string.IsNullOrEmpty(value))
+			{
+				markdown = "[]()";
+			}
+			else
+			{
+				markdown = $"[{value}]()";
+			}
+
+			await Insert(markdown);
+			await Text.FocusAsync();
+		}
+
+		private async void OnClickBold()
+		{
+			await BothSideWithMd("**");
+		}
+
+		private async void OnClickItalic()
+		{
+			await BothSideWithMd("*");
+		}
+
+		private async void OnClickQuote()
+		{
+			await StartWithMd(">");
+		}
+
+		private async void OnClickBlockCode()
+		{
+			await BothSideWithMd("```");
+		}
+
+		private async void OnClickImage()
+		{
+			string value = await GetSelection();
+			string markdown = string.Empty;
+
+			if (string.IsNullOrEmpty(value))
+			{
+				markdown = "![]()";
+			}
+			else
+			{
+				markdown = $"![{value}]()";
+			}
+
+			await Insert(markdown);
+			await Text.FocusAsync();
+
+
+			await Insert("![]()");
+			await Text.FocusAsync();
+		}
+
+		private async Task BothSideWithMd(string markdownSymbol)
+		{
+			string value = await GetSelection();
+			string markdown = string.Empty;
+
+			if (string.IsNullOrEmpty(value))
+			{
+				markdown = $"{markdownSymbol} {markdownSymbol}";
+			}
+			else
+			{
+				markdown = markdownSymbol + value + markdownSymbol;
+			}
+
+			await Insert(markdown);
+			await Text.FocusAsync();
+		}
+
+		private async Task StartWithMd(string markdownSymbol)
+		{
+			string value = await GetSelection();
+			string markdown = string.Empty;
+
+			if (string.IsNullOrEmpty(value))
+			{
+				markdown = markdownSymbol;
+			}
+			else
+			{
+				markdown = $"{markdownSymbol} {value}";
+			}
+
+			await Insert(markdown);
+			await Text.FocusAsync();
+		}
+
+
+		private async Task Insert(string markdown)
+		{
+			await JSRuntime.InvokeVoidAsync("insertAtCursor", "texteditor", markdown);
+		}
+
+		private async Task<string> GetSelection()
+		{
+			try
+			{
+				return await JSRuntime.InvokeAsync<string>("getSelectedText", "texteditor");
+			}
+			catch (Exception ex)
+			{
+				var test = ex;
+				throw;
+			}
+		}
+	}
+}
---
+++ src/BlazorBlog/wwwroot/scripts/blog.js
@@ -0,0 +1,27 @@
+﻿function insertAtCursor(id, myValue) {
+    var myField = document.getElementById(id);
+
+    if (myField.selectionStart || myField.selectionStart == '0') {
+	
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+		
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+			
+    } else {
+        myField.value += myValue;
+    }
+}
+
+function getSelectedText(id) {
+    var selectedText = '';
+
+    var myTextAera = document.getElementById(id);
+    var startPos = myTextAera.selectionStart;
+    var endPos = myTextAera.selectionEnd;
+    var selectedText = myTextAera.value.substring(startPos, endPos);
+
+    return selectedText;
+}
